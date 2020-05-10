22e501159bcb1d6b66e80ea00d482eeaeed77937
Steve Idowu <idowusteve@ymail.com>
2020-05-10 22:57:36 +0100

    Timeline --Tweets API

---
+++ app/Http/Resources/TweetCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\TweetResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TweetCollection extends ResourceCollection
+{
+    /**
+     * Undocumented variable
+     *
+     * @var [type]
+     */
+    public $collects = TweetResource::class;
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection
+        ];
+    }
+}
--- app/Tweet.php
+++ app/Tweet.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    //
+    /**
+     * Tweets user
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
