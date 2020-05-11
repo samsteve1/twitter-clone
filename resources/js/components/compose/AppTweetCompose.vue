319b78e3a02fdcefcbbaef747dd589f0db9db109
Steve Idowu <idowusteve@ymail.com>
2020-05-11 02:42:57 +0100

    Composing Tweets - Form styles

---
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <form class="flex">
+      <div class="mr-3">
+        <img :src="$user.avatar" class="w-12 rounded-full" />
+      </div>
+      <div class="flex-grow">
+        <textarea
+          class="bg-gray-900 w-full outline-none text-lg resize-none mb-2"
+          placeholder="What's happening"
+          autofocus
+        ></textarea>
+        <div class="flex justify-between">
+            <div>
+                actions
+            </div>
+            <div>
+                <button type="submit" class="bg-blue-500 rounded-full text-gray-300 text-center px-4 py-3 font-bolf leading-none">Tweet</button>
+            </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
